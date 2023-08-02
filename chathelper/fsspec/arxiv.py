2bd85578500059d96db2f74c5184a3aa3017f3f6
Gordon Watts <gwatts@uw.edu>
2023-08-02 18:16:42 +0200

    Implementing the arxiv fsspec

---
+++ chathelper/fsspec/arxiv.py
@@ -0,0 +1,56 @@
+from typing import Any, Dict
+from urllib.parse import urlparse
+from fsspec import AbstractFileSystem
+import fsspec
+from pydantic_core import Url
+import requests
+import functools
+
+
+class ArxivFileSystem(AbstractFileSystem):
+    def _get_paper_url(self, path: str) -> Url:
+        """Get the URL of the paper from the metadata
+
+        Args:
+            path (str): The path from `fsspec`
+
+        Returns:
+            Url: The URL of the paper
+        """
+        uri = urlparse(path)
+        return Url(f"https://arxiv.org/pdf/{uri.netloc}.pdf")
+
+    def ls(self, path, detail=True, **kwargs):
+        """Get metadata info for the given path. Cached.
+
+        Args:
+            path (str): Path to the file
+            detail (bool, optional): How many details. Defaults to True.
+
+        Returns:
+            Dict[]: Keys needed for fsspec
+        """
+        meta = {}
+        meta["name"] = path
+        meta["size"] = None
+        meta["type"] = "file"
+        return [meta]
+
+    def open(
+        self,
+        path,
+        mode="rb",
+        block_size=None,
+        cache_options=None,
+        compression=None,
+        **kwargs,
+    ):
+        if mode != "rb":
+            raise NotImplementedError("Only read-byte mode is supported ('rb')")
+
+        # Grab the URL of the document and open it with fsspec's http backend.
+        doc_url = self._get_paper_url(path)
+        source_fs = fsspec.filesystem(doc_url.scheme)
+        return source_fs.open(
+            str(doc_url), mode, block_size, cache_options, compression
+        )
---
+++ tests/fsspec/test_arxiv.py
@@ -0,0 +1,15 @@
+import fsspec
+from chathelper.fsspec.arxiv import ArxivFileSystem
+
+
+def test_copy(tmp_path):
+    output_file = tmp_path / "2203.01009.pdf"
+    fs = ArxivFileSystem()
+    fs.get("arxiv://2203.01009", str(output_file))
+
+    assert output_file.exists()
+
+
+def test_find_protocol():
+    fs = fsspec.filesystem("arxiv")
+    assert isinstance(fs, ArxivFileSystem)
