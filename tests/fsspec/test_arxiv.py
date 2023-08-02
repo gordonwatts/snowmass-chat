85109e65a2558ae008dd2332707c03ab2ce96d9c
Gordon Watts <gwatts@uw.edu>
2023-08-02 18:19:36 +0200

    Skip expensive download tests till mocking is done

--- tests/fsspec/test_arxiv.py
+++ tests/fsspec/test_arxiv.py
@@ -1,7 +1,9 @@
 import fsspec
 from chathelper.fsspec.arxiv import ArxivFileSystem
+import pytest
 
 
+@pytest.mark.skip("Network hit")
 def test_copy(tmp_path):
     output_file = tmp_path / "2203.01009.pdf"
     fs = ArxivFileSystem()
--- tests/fsspec/test_inspirehep.py
+++ tests/fsspec/test_inspirehep.py
@@ -1,7 +1,9 @@
 import fsspec
 from chathelper.fsspec.inspirehep import InspireFileSystem
+import pytest
 
 
+@pytest.mark.skip("Network hit")
 def test_copy(tmp_path):
     output_file = tmp_path / "2043503.pdf"
     fs = InspireFileSystem()
